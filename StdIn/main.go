3d2c6a3e5c4aa6a6a4cd522d90e26cf32dcf6686
mutembeijoe <mutembeijoe@gmail.com>
2020-05-17 19:43:33 +0300

    adds scan stdin

---
+++ StdIn/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+var choice int
+
+func main(){
+	fmt.Println("What is a Square?")
+	fmt.Print("Enter 1=Quadrilateral 2=Rectahedron :")
+
+	n, err:= fmt.Scanf("%d", &choice)
+
+	if n != 1 || err != nil{
+		fmt.Print("Follow the Instructions!")
+		return
+	}
+
+	if choice == 1{
+		fmt.Println("Correct!")
+	}else{
+		fmt.Println("Failed, Google it")
+	}
+}
