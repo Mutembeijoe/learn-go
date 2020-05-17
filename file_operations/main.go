8b8105037a61d874def7080b69f029c667d7b421
mutembeijoe <mutembeijoe@gmail.com>
2020-05-17 17:05:21 +0300

    adds sample code for file operations with os and io packages

---
+++ file_operations/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+func main() {
+	rf, err := os.Open("./data.txt")
+
+	if err != nil {
+		fmt.Println("Failed to open file", err)
+		os.Exit(1)
+	}
+
+	defer rf.Close()
+
+	wf, err := os.Create("./write_file")
+
+	if err != nil {
+		fmt.Println("Failed to Create file", err)
+		os.Exit(1)
+	}
+
+	defer wf.Close()
+
+	n, err := io.Copy(wf, rf)
+
+	if err != nil {
+		fmt.Println("Failed to copy file data")
+		os.Exit(1)
+	}
+
+	fmt.Printf("Copied %d from %s to %s ", n, rf.Name(), wf.Name())
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mutembeijoe/data_io
+
+go 1.14
