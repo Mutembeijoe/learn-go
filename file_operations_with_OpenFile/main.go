40926e8d5e623be0794ea9d4fa97d1d4df8097d4
mutembeijoe <mutembeijoe@gmail.com>
2020-05-17 17:40:28 +0300

    adds read Operations with OpenFile

---
+++ file_operations_with_OpenFile/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+func main() {
+	rf, err := os.OpenFile("./write_only.txt", os.O_RDONLY, 0666)
+
+	if err != nil {
+		fmt.Println("failed to Open file", err)
+		os.Exit(1)
+	}
+
+	defer rf.Close()
+
+	newReader := strings.NewReader("Hello World, This is a strings reader")
+
+	n, err := io.Copy(rf, newReader)
+
+	if err != nil {
+		fmt.Println("Failed to copy data", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Copied %d bytes from %s to StdOut", n, rf.Name())
+
+}
