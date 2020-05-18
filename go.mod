e3cd3d6ce441e992a83b3dd2020812f367f440ee
mutembeijoe <mutembeijoe@gmail.com>
2020-05-18 14:56:29 +0300

    adds a basic gorilla mux routing illustration

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/mutembeijoe/data_io
 
 go 1.14
 
-require github.com/julienschmidt/httprouter v1.3.0
+require (
+	github.com/gorilla/mux v1.7.4
+	github.com/julienschmidt/httprouter v1.3.0
+)
---
+++ net_http/gorilla mux/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+func getBook(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "\t Book category : %v \n\t Book id : %v", vars["category"], vars["id"])
+}
+
+func main() {
+	gMux := mux.NewRouter()
+
+	gMux.HandleFunc("/api/books/{category}/{id:[0-9]+}", getBook)
+
+	s := &http.Server{
+		Addr:         ":8080",
+		WriteTimeout: 10 * time.Second,
+		ReadTimeout:  10 * time.Second,
+		Handler:      gMux,
+	}
+
+	log.Fatal(s.ListenAndServe())
+}
