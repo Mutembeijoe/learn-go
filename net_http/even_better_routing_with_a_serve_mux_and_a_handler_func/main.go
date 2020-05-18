78b1fef6082e4f23e52385877142a90a09538417
mutembeijoe <mutembeijoe@gmail.com>
2020-05-18 11:49:57 +0300

    adds illustration on the usage of a http.HandlerFunc

--- net_http/even_better_routing_with_a_serve_mux_and_a_handler_func/main.go
+++ net_http/even_better_routing_with_a_serve_mux_and_a_handler_func/main.go
@@ -5,17 +5,22 @@ import (
 )
 
 func handlerFunc1(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("This response is written from handler fucn 1"))
+	w.Write([]byte("This response is written from handler func 1"))
 }
 
 func handlerFunc2(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("This response is written from handler fucn 2"))
+	w.Write([]byte("This response is written from handler func 2"))
 }
 
 func main() {
 	mux := http.NewServeMux() //a default serve-mux would work here too
 
-	mux.HandleFunc("/route1", handlerFunc1)
+	// A http.HandlerFunc converts any function with the signature
+	// func(w http.ResponseWrite, r *http.Request){}
+	// to a handlerFunc which implements the ServeHttp method thus making it a handler.
+	//Notice how the mux.Handle vs mux.HandleFunc are used below
+
+	mux.Handle("/route1", http.HandlerFunc(handlerFunc1))
 	mux.HandleFunc("/route2", handlerFunc2)
 
 	http.ListenAndServe(":8080", mux)
